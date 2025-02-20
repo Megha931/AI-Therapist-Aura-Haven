ae1d73c24d85fac1a238adb81d7d21947915eb49
Megha <121974968+Megha931@users.noreply.github.com>
2025-02-20 14:44:11 +0530

    Add files via upload

---
+++ frontend/src/helpers/api-communicator.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export const loginUser = async (email: string, password: string) => {
+  const res = await axios.post("/user/login", { email, password });
+  if (res.status !== 200) {
+    throw new Error("Unable to login");
+  }
+  return res.data;
+};
+
+export const signupUser = async (name: string, email: string, password: string) => {
+  const res = await axios.post("/user/signup", { name, email, password });
+  if (res.status !== 201) {
+    throw new Error("Unable to Signup");
+  }
+  return res.data;
+};
+
+export const checkAuthStatus = async () => {
+  const res = await axios.get("/user/auth-status");
+  if (res.status !== 200) {
+    throw new Error("Unable to authenticate");
+  }
+  return res.data;
+};
+
+export const sendChatRequest = async (message: string) => {
+  const res = await axios.post("/chat/new", { message });
+  if (res.status !== 200) {
+
+    throw new Error("Unable to send chat");
+
+  }
+  return res.data; // Ensure backend response format matches expected structure
+};
+
+export const getUserChats = async () => {
+  const res = await axios.get("/chat/all-chats");
+  if (res.status !== 200) {
+    throw new Error("Unable to fetch chats");
+  }
+  return res.data;
+};
+
+export const deleteUserChats = async () => {
+  const res = await axios.delete("/chat/delete");
+  if (res.status !== 200) {
+    throw new Error("Unable to delete chats");
+  }
+  return res.data;
+};
+
+export const logoutUser = async () => {
+  const res = await axios.get("/user/logout");
+  if (res.status !== 200) {
+    throw new Error("Unable to logout");
+  }
+  return res.data;
+};
