14fc73fe515dcfea7137829cbe7f8309d89c36b6
Megha <121974968+Megha931@users.noreply.github.com>
2025-02-22 15:46:46 +0530

    Update validators.js

--- backend/dist/utils/validators.js
+++ backend/dist/utils/validators.js
@@ -14,6 +14,7 @@ export const validate = (validations) => {
         return res.status(422).json({ errors: errors.array() });
     };
 };
+
 export const loginValidator = [
     body("email").trim().isEmail().withMessage("Email is required"),
     body("password")
@@ -21,10 +22,13 @@ export const loginValidator = [
         .isLength({ min: 6 })
         .withMessage("Password should contain atleast 6 characters"),
 ];
+
 export const signupValidator = [
     body("name").notEmpty().withMessage("Name is required"),
     ...loginValidator,
 ];
+
+
 export const chatCompletionValidator = [
     body("message").notEmpty().withMessage("Message  is required"),
 ];
